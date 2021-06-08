9fe0bba7c7a0930693460d52d01ac99716aa19f8
Lucas Dietrich <ld.adecy@gmail.com>
2021-06-09 00:08:56 +0200

    added datetime update

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import time
 import random
 
+import struct
+
 import can
 
 import datetime;
@@ -15,6 +17,34 @@ can_ifs = [
     "can1"
 ]
 
+msgs = [
+    MsgId(
+        frame_type=MsgId.FrameType.WriteAttribute,
+        query_type=MsgId.QueryType.Query,
+        controller=MsgId.Controller.Controller1,
+        device_id=DeviceId(DeviceId.DataType.CRT, 1)
+    ),
+    MsgId(
+        frame_type=MsgId.FrameType.WriteAttribute,
+        query_type=MsgId.QueryType.Query,
+        controller=MsgId.Controller.Controller1,
+        device_id=DeviceId(DeviceId.DataType.CRT, 0)
+    ),
+    MsgId(
+        frame_type=MsgId.FrameType.WriteAttribute,
+        query_type=MsgId.QueryType.Query,
+        controller=MsgId.Controller.Controller1,
+        device_id=DEVICE_BROADCAST
+    )
+]
+
+# send_msg = can.Message(
+#     arbitration_id=int(msgs[i % 3]),
+#     data=generate_random_data(8),
+#     is_extended_id=False,
+#     timestamp=datetime.datetime.now().timestamp()
+# )
+
 if __name__ == "__main__":
 
     for can_if in can_ifs:
@@ -22,44 +52,37 @@ if __name__ == "__main__":
 
     canb = can.interface.Bus(channel=can_ifs[1], bustype='socketcan')
 
-    msgs = [
-        MsgId(
-            frame_type=MsgId.FrameType.WriteAttribute,
-            query_type=MsgId.QueryType.Query,
-            controller=MsgId.Controller.Controller1,
-            device_id=DeviceId(DeviceId.DataType.CRT, 1)
-        ),
-        MsgId(
-            frame_type=MsgId.FrameType.WriteAttribute,
-            query_type=MsgId.QueryType.Query,
-            controller=MsgId.Controller.Controller1,
-            device_id=DeviceId(DeviceId.DataType.CRT, 0)
-        ),
-        MsgId(
-            frame_type=MsgId.FrameType.WriteAttribute,
-            query_type=MsgId.QueryType.Query,
-            controller=MsgId.Controller.Controller1,
-            device_id=DEVICE_BROADCAST
-        )
-    ]
-
     i = 0
 
     while True:
-        send_msg = can.Message(
-            arbitration_id=int(msgs[i % 3]),
-            data=generate_random_data(8),
-            is_extended_id=False, 
-            timestamp=datetime.datetime.now().timestamp()
-        )
-
-        i += 1
 
         try:
-            canb.send(send_msg)
-            print(f"can message sent on \t{canb.channel_info} : {send_msg}")
+            timestamp = datetime.datetime.now().timestamp()
+            timestamp_sec = int(timestamp)
+
+            timestamp_sec_uint32_t = struct.pack("<L", timestamp_sec)
+
+            key = struct.pack("h", 0x0101)
+
+            write_datetime = can.Message(
+                arbitration_id=int(MsgId(
+                    frame_type=MsgId.FrameType.WriteAttribute,
+                    query_type=MsgId.QueryType.Query,
+                    controller=MsgId.Controller.Controller1,
+                    device_id=DeviceId(DeviceId.DataType.CRT, 0)
+                )),
+                data=key + timestamp_sec_uint32_t,
+                is_extended_id=False,
+                timestamp=timestamp
+            )
+            
+
+            canb.send(write_datetime)
+            print(f"can message sent on \t{canb.channel_info} : {write_datetime}")
         except can.CanError:
             print("can message NOT sent : {msg}")
+
+        
 
         while True:
             recv_msg = canb.recv(timeout=1.0)
@@ -69,4 +92,4 @@ if __name__ == "__main__":
             else:
                 break
 
-        time.sleep(1.0)
+        time.sleep(5.0)
