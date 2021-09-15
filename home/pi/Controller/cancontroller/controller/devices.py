248f6a0e3d1e46baf520ced292ab94adc814ec51
Lucas Dietrich <ld.adecy@gmail.com>
2021-09-15 09:38:34 +0200

    removed unused directory

--- home/pi/Controller/cancontroller/controller/devices.py
+++
@@ -1,8 +0,0 @@
-from cancontroller.caniot import MsgId, DeviceId
-
-from typing import Dict, List, Union
-
-KNOWN_DEVICES: Dict[str, DeviceId] = {
-    "garagedoor_dev": DeviceId(DeviceId.DataType.CRT, 0),
-    "garagedoor_prod": DeviceId(DeviceId.DataType.CRT, 1)
-}
