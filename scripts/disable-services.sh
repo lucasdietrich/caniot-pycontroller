63d8327ff8fd5741b02bc1240885b43331f02a08
Lucas Dietrich <ld.adecy@gmail.com>
2021-09-26 15:01:46 +0200

    updated scripts

---
+++ scripts/disable-services.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo systemctl disable canhttpserver.service
+sudo systemctl disable cancontroller.service
--- scripts/start-services.sh
+++ scripts/start-services.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+sudo systemctl enable canhttpserver.service
+sudo systemctl enable cancontroller.service
+
 sudo systemctl start canhttpserver.service
 sudo systemctl start cancontroller.service
