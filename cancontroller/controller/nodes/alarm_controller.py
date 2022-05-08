89d8dbd80170151fc5cfa5a2bb2ec0af72da0bb3
Lucas Dietrich <ld.adecy@gmail.com>
2022-05-08 19:17:11 +0200

    Fix grpc server getting stuck ?

--- cancontroller/controller/nodes/alarm_controller.py
+++ cancontroller/controller/nodes/alarm_controller.py
@@ -34,7 +34,8 @@ class AlarmController(CustomPcb_Node):
                 self.model['last_signal'] = now
 
                 if self.model['enabled']:
-                    api.BoardLevelCommand(self.deviceid, coc1=XPS.PULSE_ON,
-                                          coc2=XPS.PULSE_ON, crl1=XPS.SET_NONE)
+                    cmd = BoardLevelCommand(self.deviceid, coc1=XPS.PULSE_ON,
+                                            coc2=XPS.PULSE_ON, crl1=XPS.SET_NONE)
+                    print(BoardLevelCommand, cmd)
 
         return cmd
